b2c9227e56952e368f6fb3c1a1515949acb40294
joseph vilca <jo.seph-fck-95@hotmail.com>
2019-11-07 15:50:39 -0300

    agregado posicion

--- src/modelo/Tablero.java
+++ src/modelo/Tablero.java
@@ -1,14 +1,62 @@
 package modelo;
+import java.util.*;
 
 public class Tablero {
 
-	UnidadDeJuego [][]casillas;
-	
+	protected HashMap<Posicion, UnidadDeJuego> casillas;
+
 	public Tablero(){
 		
-		this.casillas = new UnidadDeJuego[20][20];
+		this.casillas = new HashMap<Posicion, UnidadDeJuego>();
 		
 	}
+
+	public boolean posicionInvalida(Posicion unaPosicion){
+
+		boolean filtro1 = this.casillas.containsKey(unaPosicion);
+		boolean filtro2 = (!unaPosicion.estaEnLimmites(0, 0, 20,20));
+		
+		return (filtro1 || filtro2);
+	}
+	
+	
+	public void posicionarUnidad(Posicion posicionDeUnidad, UnidadDeJuego unidad){
+		if(this.posicionInvalida(posicionDeUnidad)) return;
+		this.casillas.put(posicionDeUnidad, unidad);
+	}
+	
+
+	public void moverUnidadAdelante(Posicion posicionDeUnidad){
+		
+		UnidadDeJuego unidad =  this.casillas.get(posicionDeUnidad);
+		this.casillas.remove(posicionDeUnidad);
+		this.casillas.put(unidad.arriba(), unidad);
+		
+	}
+	
+	public void moverUnidadAtras(Posicion posicionDeUnidad){
+		
+		UnidadDeJuego unidad =  this.casillas.get(posicionDeUnidad);
+		this.casillas.remove(posicionDeUnidad);
+		this.casillas.put(unidad.abajo(), unidad);
+		
+	}
+	
+	public void moverUnidadDerecha(Posicion posicionDeUnidad){
+		UnidadDeJuego unidad =  this.casillas.get(posicionDeUnidad);
+		this.casillas.remove(posicionDeUnidad);
+		this.casillas.put(unidad.derecha(), unidad);
+		
+	}
+	
+	public void moverUnidadIzquierda(Posicion posicionDeUnidad){
+		UnidadDeJuego unidad =  this.casillas.get(posicionDeUnidad);
+		this.casillas.remove(posicionDeUnidad);
+		this.casillas.put(unidad.izquierda(), unidad);
+		
+	}
+		
+	
 	
 	
 }
--- src/modelo/UnidadDeJuego.java
+++ src/modelo/UnidadDeJuego.java
@@ -3,6 +3,26 @@ package modelo;
 public abstract class UnidadDeJuego {
 
 	protected int vida, costo;
+	public Posicion posicion;
 	
-	public abstract void recibirDano();	
+	public abstract void recibirDano();
+	
+	public Posicion derecha(){
+		return posicion.getPosicionDerecha();
+	}
+	
+	public Posicion izquierda(){
+		return posicion.getPosicionIzquierda();
+	}
+	
+	public Posicion abajo(){
+		return posicion.getPosicionAbajo();
+	}
+	
+	public Posicion arriba(){
+		return posicion.getPosicionArriba();
+	}
+	
+	
+	
 }
